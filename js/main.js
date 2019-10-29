565cb619d49151f68837a5d8bf2d341bc8e081ee
Imran Khan <imhysoft@gmail.com>
2019-10-30 00:50:44 +0500

    user feeds page updated

--- js/main.js
+++ js/main.js
@@ -1,4 +1,14 @@
 $(document).ready(function(){
+  $('.search-input-grp input[type="text"]').on('input propertychange', function() {
+    var $this = $(this);
+    var visible = Boolean($this.val());
+    $this.siblings('.fas').toggleClass('d-none', !visible);
+  }).trigger('propertychange');
+
+  $('.search-input-grp .fas').click(function() {
+    $(this).siblings('input[type="text"]').val('')
+      .trigger('propertychange').focus();
+  });
   $('#list').click(function(event){
     event.preventDefault();
     $('#feeds .item').addClass('list-item');
